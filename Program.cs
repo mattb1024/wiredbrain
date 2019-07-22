b131c09424f3c6a39c0a65e51f6fd62919b9a107
Matt Baha <Matt.Baha@bd.com>
2019-07-22 16:00:12 -0700

    new class and i/o

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using WiredBrain.DataAccess;
 
 namespace WiredCoffeeConsoleTool
 {
@@ -7,6 +8,29 @@ namespace WiredCoffeeConsoleTool
         static void Main(string[] args)
         {
             Console.WriteLine("Wired Brain Coffee Tool");
+
+            Console.WriteLine("Write 'help' to list available commands");
+
+            var coffeeShopDataProvider = new CoffeeShopDataProvider();
+
+            while(true)
+            {
+                var line = Console.ReadLine();
+
+                var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
+
+                if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("> Available coffee shop commands:");
+                    foreach(var coffeeShop in coffeeShops)
+                    {
+                        Console.WriteLine($"> " + coffeeShop.Location);
+                    }
+                }
+
+
+            }
+
         }
     }
 }

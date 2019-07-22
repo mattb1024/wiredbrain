f49947a8fb356b513bb333f451afdaeb25e4fc69
Matt Baha <Matt.Baha@bd.com>
2019-07-22 10:12:34 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredCoffeeConsoleTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wired Brain Coffee Tool");
+        }
+    }
+}
